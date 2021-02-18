af40862b82b35d78a05d74936d914b88e790761b
Pedro J. Pérez <pedro.j.perez@uv.es>
2021-02-18 13:51:43 +0100

    todo a Github

---
+++ _cosas_todo.R
@@ -0,0 +1,8 @@
+#- cosas a hacer -----------------------------------------
+
+#- q los chunks de código no se salgan
+
+#- enterarme bien de lo de code_folding: https://github.com/rstudio/distill/issues/160   https://github.com/ijlyttle/codefolder
+
+#- Mirar las preguntas Distill en RStudio: https://community.rstudio.com/tag/distill
+
