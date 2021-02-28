6f9d26cdb14dbcb46f2d5cbb4b168f45115341e1
Pedro J. Pérez <pedro.j.perez@uv.es>
2021-02-28 20:09:40 +0100

    quinto post padron

--- _cosas_todo.R
+++
@@ -1,8 +0,0 @@
-#- cosas a hacer -----------------------------------------
-
-#- q los chunks de código no se salgan
-
-#- enterarme bien de lo de code_folding: https://github.com/rstudio/distill/issues/160   https://github.com/ijlyttle/codefolder
-
-#- Mirar las preguntas Distill en RStudio: https://community.rstudio.com/tag/distill
-
